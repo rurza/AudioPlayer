db9b70adfce2f64bfb4aecbbd3c43bc80c5d7222
Adam Różyński <adam@micropixels.pl>
2019-07-17 10:24:20 +0200

    updated podspec

---
+++ Example/Pods/Target Support Files/Pods-StreamPlayer_Tests/Pods-StreamPlayer_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StreamPlayer_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_StreamPlayer_Tests
+@end
---
+++ Example/Pods/Target Support Files/StreamPlayer/StreamPlayer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_StreamPlayer : NSObject
+@end
+@implementation PodsDummy_StreamPlayer
+@end
